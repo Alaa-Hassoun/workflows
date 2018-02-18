a3cf1f75d97fa01ef7d5216ba91ff4c619a0c42e
hassoun alaa <a.hassoun>
2018-02-18 13:32:32 +0000

    added compass process

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
      gutil = require('gulp-util'),
      coffee = require('gulp-coffee'),
      browserify = require('gulp-browserify'),
+     compass = require('gulp-compass'),
      concat = require('gulp-concat');
 
  var coffeeSources = ['components/coffee/tagline.coffee'];
@@ -9,7 +10,9 @@
  var jsSources = ['components/scripts/rclick.js', 
                   'components/scripts/pixgrid.js', 
                   'components/scripts/template.js',
-                  'components/scripts/tagline.js'];     
+                  'components/scripts/tagline.js'];
+
+ var sassSources = ['components/sass/style.scss'];                      
 
 
  gulp.task('coffee', function() {
@@ -25,3 +28,14 @@
         .pipe(browserify())
         .pipe(gulp.dest('builds/development/js'))
  });
+
+ gulp.task('compass', function() {
+    gulp.src(sassSources)
+       .pipe(compass({
+           sass: 'components/sass',
+           image: 'builds/development/images',
+           style: 'expanded'
+       }))
+       .on('error', gutil.log)
+       .pipe(gulp.dest('builds/development/css'))
+});
